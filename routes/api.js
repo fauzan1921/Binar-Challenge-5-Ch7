9f8eea06b0e85b2a310283a3c474952cf898382c
Fauzan Aslam Kusumah <107337182+fauzan1921@users.noreply.github.com>
2022-09-26 20:10:04 +0700

    Add files via upload

--- routes/api.js
+++ routes/api.js
@@ -24,7 +24,7 @@ customMiddleware.validatePlayer, apiController.chooseRoom)
 //untuk mengirim roomId user yang sudah login agar bisa masuk ke laman room
 router.get('/room-id', jsonParser, passport.authenticate('jwt', { session: false }), apiController.getRoomId)
 
-//untuk register user sebagai player 1 atau 2, dan juga untuk menentukan player choice (R = rock, P = paper, S = scissors)
+//untuk memilih room, register user sebagai player 1 atau 2, dan juga untuk menentukan player choice
 router.put('/room/:id', jsonParser, passport.authenticate('jwt', { session: false }), 
 customMiddleware.validatePlayer, apiController.gameRoom)
 
